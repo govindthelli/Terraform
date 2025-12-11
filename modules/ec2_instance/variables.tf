b941efd95d7a9816ffe89aa83e96201d611223eb
govindthelli <govindthelli560@gmail.com>
2025-12-11 15:21:15 +0530

    push modules folder

---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+    description = "Enter ami"
+    type = string
+}
+
+variable "subnet" {
+    description = "Enter subnet_id"
+    type = string
+}
+
+variable "instance_type" {
+    description = "Enter instance type"
+    type = string
+}
